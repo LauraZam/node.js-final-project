281fd60ec2771c3c407f1cb00a36136de292a0cb
laurazam <77395682+LauraZam@users.noreply.github.com>
2021-07-23 14:32:30 +0600

    a

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const pcRouter = require("./routes/pcRouter")
 const bodyParser = require("body-parser")
+const fs = require("fs")
+const qs = require('querystring');
 const app = express()
 const hbs = require("hbs")
 app.use(bodyParser.json());
@@ -9,36 +11,23 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.set("view engine", "hbs")
 hbs.registerPartials(__dirname + "/views/partials")
 app.use("/pc", pcRouter)
-app.use("/pc/db", function (request, response) {
-    if (request.method === "GET") {
-        response.render("db.hbs")
-    }else if (request.method === "POST") {
-        var body = '';
-
-        request.on('data', function (data) {
-            body += data;
-
-            if (body.length > 1e6)
-                request.connection.destroy();
-        });
-        request.on('end', function () {
-            let post = qs.parse(body)
-            let username = post.username;
-            let price = post.price;
-            let newUser = {
-                username: username,
-                price: price
-            }
-            let usersFileContent = fs.readFileSync("./db/users.txt");
-            let usersArray = null;
-            try {
-                usersArray = JSON.parse(usersFileContent)
-            } catch {
-                usersArray = []
-            }
-            usersArray.push(newUser);
-            fs.writeFileSync("./db/users.txt", JSON.stringify(usersArray))
-        });
+app.get("/pc/db", function (request, response) {
+    response.render("db.hbs")
+})
+app.post("/pc/db", function (request, response) {
+    let username = request.body.username;
+    let price = request.body.price;
+    console.log(request);
+    let fileContent = fs.readFileSync("./db/users.txt")
+    let usersArray = JSON.parse(fileContent)
+    let newUser = {
+        username: username,
+        price: price
     }
+    usersArray.push(newUser);
+
+    fs.writeFileSync("./db/users.txt", JSON.stringify(usersArray))
+
+    response.redirect("/pc");
 })
 app.listen(8080)
---
+++ helpers/tb.js
@@ -0,0 +1,24 @@
+const TelegramApi=require('node-telegram-bot-api')
+
+const token ='1912952094:AAGtzz5g6im5Nrs8zf0PFfEVxCZ3bT3lJKA'
+
+const bot=new TelegramApi(token, {polling: true})
+
+bot.on('message', msg => {
+    const text=msg.text;
+    const chatid=msg.chat.id;
+    
+    // if(text=== '/start'){
+    //     bot.sendMessage(chatid, `привет, ${msg.from.first_name}`)
+    // }
+   
+    // if(text=== '/info'){
+    //     bot.sendMessage(chatid, `тебе , ${msg.from.age}`)
+    // }
+    if (text === username) {
+        bot.sendMessage(username);
+        bot.sendMessage(price)
+    }
+
+    
+})
